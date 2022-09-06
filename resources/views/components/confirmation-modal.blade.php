4c8d3e1f3953e30d9ca3a8ca0b89041fc097937c
huynguyen <nguyenlehuyuit@gmail.com>
2022-09-06 13:32:01 +0700

    Continue

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,4 +1,5 @@
-<div x-data="{isShow:false}" x-show="isShow">
+@props(['name'])
+<div style="display: none" x-data="{isShow:false}" x-show="isShow" x-cloak @hashchange.window="isShow = location.hash === '#{{$name}}'">
     <div class="fixed inset-0 bg-gray-900 opacity-90">
     </div>
     <div class="bg-white fixed flex flex-col h-48 inset-0 m-auto max-w-sm px-4 py-2 rounded-md shadow-md space-y-7">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container max-w-lg mx-auto bg-gray-300">
+    <div class="container max-w-lg mx-auto bg-gray-300" x-data="{confirmed: false}">
         <header class="bg-blue-600 p-4">
             <h1 class="font-bold text-white">My Site</h1>
         </header>
@@ -14,15 +14,10 @@
             </aside>
 
             <main class="text-sm col-span-9">
-                <p class="mb-4">
-                    Exercitation commodo velit duis reprehenderit anim duis ullamco et ut cillum officia nostrud.
-                </p>
-                <p class="mb-4">
-                    Pariatur eu ad ad dolor esse mollit sunt esse esse.
-                </p>
-                <p>
-                    Eiusmod laborum duis et ipsum occaecat aliquip minim nulla consequat esse nisi voluptate.
-                </p>
+                <form id="delete-modal" action="" method="post" @submit.prevent="if (!confirmed) location.hash='#user-delete-modal'">
+                    <button class="px-4 py-2 rounded bg-blue-400 text-white">Delete modal?</button>
+                </form>
+
             </main>
         </div>
 
@@ -31,7 +26,7 @@
             <p>2021</p>
         </footer>
     </div>
-    <x-confirmation-modal>
+    <x-confirmation-modal name="user-delete-modal">
         <x-slot name="title">
             Are you sure?
         </x-slot>
@@ -39,9 +34,26 @@
             if you proceed, your account will be deleted forever
         </x-slot:body>
         <x-slot:footer>
-            <x-button class="bg-blue-400 hover:bg-blue-500 mr-2">Continue</x-button>
-            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
+            <x-button @click="document.querySelector('#delete-modal').submit()" class="bg-blue-400 hover:bg-blue-500 mr-2">Continue</x-button>
+{{--            <a href="#something-else" class = 'bg-blue-400 hover:bg-blue-500 mr-2 px-4 py-2 rounded-xl text-white transition'>Continue</a>--}}
+            <a href="#cancel" class = 'bg-gray-400 hover:bg-gray-500 px-4 py-2 rounded-xl text-white transition'>Cancel</a>
+{{--            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>--}}
         </x-slot:footer>
     </x-confirmation-modal>
+
+{{--    <x-confirmation-modal name="something-else">--}}
+{{--        <x-slot name="title">--}}
+{{--            Just checking--}}
+{{--        </x-slot>--}}
+{{--        <x-slot:body>--}}
+{{--            if you proceed, your account will be deleted forever--}}
+{{--        </x-slot:body>--}}
+{{--        <x-slot:footer>--}}
+{{--            <a href="#user-delete-modal" class = 'bg-blue-400 hover:bg-blue-500 mr-2 px-4 py-2 rounded-xl text-white transition'>Retry</a>--}}
+{{--            <x-button class="bg-blue-400 hover:bg-blue-500 mr-2">Retry</x-button>--}}
+{{--            <a href="#cancel" class = 'bg-gray-400 hover:bg-gray-500 px-4 py-2 rounded-xl text-white transition'>Cancel</a>--}}
+{{--            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>--}}
+{{--        </x-slot:footer>--}}
+{{--    </x-confirmation-modal>--}}
 
 </x-layout>
